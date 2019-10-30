1a0e793c7d1d54d78b7b8af8c7230a3aa6b1a1d6
15049420781@163.com <15049420781@163.com>
2019-10-31 06:38:32 +0800

    更新

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
 import memoryUtils from '../../utils/memoryUtils';
+import LeftNav from '../../components/lef-nav';
+import Header from '../../components/header';
+
+import Home from '../home/home';
+import Category from '../category/category';
+import Product from '../product/product';
+import Role from '../role/role';
+import User from '../user/user';
+import Bar from '../charts/bar';
+import Line from '../charts/line';
+import Pie from '../charts/pie';
+
+const { Footer, Sider, Content } = Layout;
+
 
 export default class Admin extends Component {
     render() {
@@ -16,9 +31,34 @@ export default class Admin extends Component {
         }
 
         return (
-            <div>
-                hello, {user.username}
-            </div>
+            // <div>
+            //     hello, {user.username}
+            // </div>
+
+             <Layout style={{height: '100%'}}>
+             <Sider>
+                 <LeftNav />
+             </Sider>
+             <Layout>
+               <Header />
+               <Content style={{background: 'white' }}>
+                    <Switch>
+                        <Route path="/home" component={Home}/>
+                        <Route path='/category' component={Category}/>
+                        <Route path='/product' component={Product}/>
+                        <Route path='/role' component={Role}/>
+                        <Route path='/user' component={User}/>
+                        <Route path='/charts/bar' component={Bar}/>
+                        <Route path='/charts/line' component={Line}/>
+                        <Route path='/charts/pie' component={Pie}/>
+                        <Redirect to="/home"/>
+                    </Switch>
+                </Content>
+               <Footer style={{textAlign: 'center', color: 'rgba(0, 0, 0, 0.5)'}}>
+                   推荐使用谷歌浏览器，获得更佳体验
+               </Footer>
+             </Layout>
+           </Layout>
         )
     }
 }
